0c71863bf5a0377b8dc7e41f16806502215113c8
hortencia718 <hortenciaecisneros@gmail.com>
2020-08-25 20:34:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # HTTPVERB "/URL", to: "controllerNAME method"
+  # GET , POST, PATCH, PUT, DELETE
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/hello_world', to: 'hw#hello_world'
 end
